aa062928a96323a7e1c24087da138802f1f0574f
Valerie <80711623+kadoys@users.noreply.github.com>
2021-08-28 11:37:59 +0300

    Add files via upload

---
+++ FirstProject.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class FirstProject {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello, what is your name?");
+        String name = scanner.nextLine();
+        System.out.println("How old are you?");
+        String old = scanner.nextLine();
+        System.out.println("What is your favourite colour");
+        String colour = scanner.nextLine();
+
+    }
+}
+
